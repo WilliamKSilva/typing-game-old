a48ee7f9f7f972d9777f3063574cb6b5ac123623
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-09-16 22:54:27 -0300

    feat: add dumb method to get JSON body from stream

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,27 @@ async fn main() {
     }
 }
 
-fn read_message_from_stream(buff: String) -> Result<(Message), Box<dyn Error>> {
-    let message: Message = serde_json::from_str(&buff).unwrap();
+// Fuck it, I need the JSON body, but the httparse crate don't give any method to get this!
+// This is a dumb approach I'am certain of that, I will find something better
+fn get_body(buff: String, body: &mut String) {
+    let mut json_start = false;
+    for c in buff.chars() {
+        if c == '}' {
+            body.push(c);
+            break;
+        }
+
+        if c == '{' {
+            json_start = true;
+            body.push(c);
+            continue;
+        }
+
+        if json_start {
+            body.push(c);
+            continue;
+        }
+    }
 }
 
 async fn process_http(stream: TcpStream) {
@@ -40,13 +59,14 @@ async fn process_http(stream: TcpStream) {
 
     stream.try_read(&mut buff_reader).unwrap();
 
-    println!("{:?}", String::from_utf8(buff_reader.clone()));
+    let mut body = String::new();
+    get_body(String::from_utf8(buff_reader.clone()).unwrap(), &mut body);
+
+    println!("{:?}", body);
     
-    assert!(req.parse(&buff_reader).unwrap().is_complete());
+    // assert!(req.parse(&buff_reader).unwrap().is_complete());
 
     let response = format!("asiudhiasuhdiuashdua");
 
-    let (mut std_stream, message) = read_message_from_stream(buff_reader).unwrap();
-    print!("{:?}", message.name);
-    let _ = std_stream.write_all(response.as_bytes()).unwrap();
+    // let _ = stream.write_all(response.as_bytes()).await.unwrap();
 }
