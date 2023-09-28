792bbe953fca0e63476f48302435a4d485d0d09a
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-09-27 22:40:21 -0300

    feat: start adding http standard library basic HTTP server functions

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,23 @@
-
+import http from "node:http"
 
 export const main = () => {
-	console.log("aiusduias")
+	const server = http.createServer()
+
+	const port = 3333
+
+	server.listen(port, () => {
+		console.log(`Server listening on port: ${3333}`)
+	})
+
+	server.on('request', (req, res) => {
+		let buff = ''
+		req.on('data', (chunk) => {
+			buff += chunk
+		})
+		req.on('end', () => {
+			console.log(buff)
+		})
+	})
 }
 
 main()
