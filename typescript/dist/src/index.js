d69346d712fa37722fe1e99d987bb71986b3a6a8
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-10-01 21:46:19 -0300

    feat: add package.json dependencies and remove dist directory from repository

--- typescript/dist/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
-const main = () => {
-    console.log("aiusduias");
-};
-exports.main = main;
-(0, exports.main)();
