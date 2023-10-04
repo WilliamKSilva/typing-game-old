9c2fd3710d62c33ce603db21b3a4bdae2461d734
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-10-03 21:11:14 -0300

    feat: change client html static page to an cool solid app

---
+++ client/src/index.tsx
@@ -0,0 +1,26 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import { Route, Router, Routes } from '@solidjs/router';
+import App from './App';
+import './index.css';
+
+const root = document.getElementById('root');
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+  );
+}
+
+render(
+  () => (
+    <Router>
+      <Routes>
+        <Route path="/" component={App}>
+          <App />
+        </Route>
+      </Routes>
+    </Router>
+  ), root!,
+);
---
+++ client/src/pages/games.tsx
@@ -0,0 +1,7 @@
+
+
+export function Games() {
+  <div>
+    <p>games daoras meu patrao</p>
+  </div>
+}
