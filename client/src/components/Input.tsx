c572e4edd5d8c048d925e475e792e5617b2c930d
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-10-23 20:53:27 -0300

    feat(client): make Input function props optional

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -6,11 +6,21 @@ type InputProps = {
   placeholder: string
   name: string
   disabled: boolean
-  onChange: (evt: any) => void
+  onChange?: (evt: any) => void
+  onKeyUp?: (evt: any) => void
+  onKeyDown?: (evt: any) => void
 }
 
 export const Input: Component<InputProps> = (props) => {
   return (
-    <input class="input" name={props.name} placeholder={props.placeholder} onChange={(evt) => props.onChange(evt)} disabled={props.disabled} />
+    <input
+      class="input"
+      name={props.name} 
+      placeholder={props.placeholder} 
+      onChange={(evt) => props.onChange ? props.onChange(evt) : {}} 
+      disabled={props.disabled} 
+      onKeyUp={(evt) => props.onKeyUp ? props.onKeyUp(evt) : {}} 
+      onKeyDown={(evt) => props.onKeyDown ? props.onKeyDown(evt) : {}} 
+    />
   )
 }
